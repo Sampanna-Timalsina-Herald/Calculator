d2098100daa700fb96de7b2f22513a583ad6ee03
ChujanTamang01 <np03cs4s240098@heraldcollege.edu.np>
2025-03-19 15:24:14 +0545

    Functionality

--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,13 @@
 #Calculator App
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiplication(x,y):
+    return x*y
+def division(x,y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return "error: Division by zero is not allowed"
+    
